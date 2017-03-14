8d8b6ecc1659f1d5bb1ead628d03b3f778a49d5c
yuyedaidao <wyqpadding@gmail.com>
2017-03-14 18:15:27 +0800

    基础功能实现，准备区分target

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "hummingbird"
+    name: "hummingbird",
+    
+    dependencies: [
+        .Package(url: "https://github.com/jatoben/CommandLine.git","3.0.0-pre1"),
+        .Package(url: "https://github.com/onevcat/Rainbow.git", majorVersion: 2)
+    ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,48 @@
-print("Hello, world!")
+import Foundation
+import CommandLineKit
+import Rainbow
+
+let cli = CommandLineKit.CommandLine()
+
+let projectOption = StringOption(shortFlag: "p", longFlag: "project", required: true, helpMessage: "Path to the project.")
+let exludePathsOption = MultiStringOption(shortFlag: "e", longFlag: "exclude", helpMessage: "Excluded paths which should not search.")
+let resourceExtensionsOption = MultiStringOption(shortFlag: "r", longFlag: "resource-extensions", helpMessage: "Extensions to search.")
+let fileExtensionsOption = MultiStringOption(shortFlag: "f", longFlag: "file-extensions", helpMessage: "File extensions to search.")
+let help = BoolOption(shortFlag: "h", longFlag: "help",
+                      helpMessage: "Prints a help message.")
+
+
+cli.addOptions(projectOption, resourceExtensionsOption, fileExtensionsOption, help)
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+    case .optionFlag:
+        str = s.green.underline
+    case .optionHelp:
+        str = s.lightBlue
+    default:
+        str = s
+    }
+    
+    return cli.defaultFormat(s: str, type: type)
+}
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+if help.value {
+    cli.printUsage()
+    exit(EX_USAGE)
+}
+
+let project = projectOption.value ?? "."
+let resourceExtensions = resourceExtensionsOption.value ?? ["png", "jpg", "imageset"]
+let fileExtensions = fileExtensionsOption.value ?? ["swift", "mm", "m", "xib", "storyboard"]
+let exludedPaths = exludePathsOption.value ?? []
+
